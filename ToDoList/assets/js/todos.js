fb3b0a35cbe2391d5f376a9da8b7d3dba5bf324f
DESKTOP-C98I1Q9\HOME <elizurtamar@gmail.com>
2020-09-10 14:28:43 +0300

    no message

---
+++ ToDoList/assets/js/todos.js
@@ -0,0 +1,22 @@
+﻿//check off a task
+$("ul").on("click","li", function () {
+    $(this).toggleClass("done");
+})
+//remove a task
+$("ul").on("click","span", function (event) {
+    event.stopPropagation();
+    $(this).parent().fadeOut(function () {
+        $(this).remove();
+    })
+})
+//adding new task
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var toDoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span>" + toDoText + "</li>");
+    }
+})
+$(".fa-pencil-alt").on("click", function () {
+    $("input[type='text']").fadeToggle();
+})
